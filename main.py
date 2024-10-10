123402fdc91a75b1000479570cd074570a1671fa
Alisson Barbosa <161394750+Alysson-b@users.noreply.github.com>
2024-10-09 22:15:24 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+numero = float(input('Insira o numero a ser verificado.'))
+
+if(numero > 0):
+    print(f'O número {numero:.2f} é maior que zero, entao ele é positivo')
+elif(numero < 0):
+    print(f'O número {numero:.2f} é menor que zero, então ele é negativo')
+else:
+    print(f'O número {numero:.2f} é zero')
